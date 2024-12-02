546f1ebac18aae1d6d41bafec5e4cdf9e1e379fd
michw85 <mvorontsov713@gmail.com>
2024-12-02 09:48:14 +0100

    setting up the database
    checking the user in the database

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,42 @@
+/**
+ * @route POST /api/user/login
+ * @desc Login
+ * @access Public
+ */
+
+const { prisma } = require("../prisma/prisma-client");
+const bcrypt = require("bcrypt");
+const e = require("express");
+const jwt = require("jsonwebtoken");
+
+// checking the user in the database
 const login = async (req, res) => {
-  res.send("login");
+  const { email, password } = req.body;
+
+  if (!email && !password) {
+    return res
+      .status(400)
+      .json({ message: "Please fill in the required fields" });
+  }
+  const user = await prisma.user.findFirst({
+    where: {
+      email,
+    },
+  });
+
+  const isPasswordCorrect =
+    user && (await bcrypt.compare(password, user.password));
+
+  // checking user password
+  if (user && isPasswordCorrect) {
+    res.status(200).json({
+      id: user.id,
+      email: user.email,
+      name: user.name,
+    });
+  }
 };
+
 const register = async (req, res) => {
   res.send("register");
 };
