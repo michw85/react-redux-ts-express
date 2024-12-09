af4413734281205aa88841a4a6862610e93b89e6
michw85 <mvorontsov713@gmail.com>
2024-12-09 15:49:42 +0100

    employees
    Controller for Employees. From model shema.prisma to client

---
+++ controllers/employees.js
@@ -0,0 +1,18 @@
+const { prisma } = require("../prisma/prisma-client");
+
+/**
+ *
+ * @route GET /api/employees
+ * @desc Get all employees
+ * @access Private
+ */
+
+const all = async (req, res) => {
+  try {
+    const employees = await prisma.employee.findMany();
+
+    res.status(200).json(employees);
+  } catch {
+    res.status(400).json({ message: "Can't get employees" });
+  }
+};
