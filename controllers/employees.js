11569bd79c15682fbbb4c35d2a53193929751d88
michw85 <mvorontsov713@gmail.com>
2024-12-26 21:05:09 +0100

    controllers_employee
    Controllers employee: add, remove, edit, get

--- controllers/employees.js
+++ controllers/employees.js
@@ -48,7 +48,83 @@ const add = async (req, res) => {
   }
 };
 
+/**
+ *
+ * @route POST /api/employees/remove/:id
+ * @desc Remove employees
+ * @access Private
+ */
+
+const remove = async (req, res) => {
+  try {
+    const { id } = req.body;
+    await prisma.employee.delete({
+      where: {
+        id,
+      },
+    });
+
+    return res.status(204).json("OK");
+  } catch {
+    // something went wrong on the server - 500
+    res.status(500).json({ message: "Failed to delete employee" });
+  }
+};
+
+/**
+ *
+ * @route PUT /api/employees/edit/:id
+ * @desc Edit employees
+ * @access Private
+ */
+
+const edit = async (req, res) => {
+  try {
+    const data = req.body;
+    const id = data.id;
+
+    await prisma.employee.update({
+      where: {
+        id,
+      },
+      data,
+    });
+
+    return res.status(204).json("OK");
+  } catch {
+    // something went wrong on the server - 500
+    res.status(500).json({ message: "Failed to edit employee" });
+  }
+};
+
+/**
+ *
+ * @route GET /api/employees/:id
+ * @desc Get employees
+ * @access Private
+ */
+
+const employee = async (req, res) => {
+  try {
+    const { id } = req.params; // http://localhost:8000/api/employees/b48f395a-e8fa-43d1-bca8-144c009df9b3
+
+    const employee = await prisma.employee.findUnique({
+      where: {
+        id,
+      },
+    });
+
+    return res.status(200).json(employee);
+  } catch {
+    // something went wrong on the server - 500
+    res.status(500).json({ message: "Failed to get employee" });
+  }
+};
+
 module.exports = {
   all,
   add,
+  remove,
+  edit,
+  employee,
 };
--- routes/employees.js
+++ routes/employees.js
@@ -1,17 +1,23 @@
 const express = require("express");
 const router = express.Router();
 const { auth } = require("../middleware/auth");
-const { all, add } = require("../controllers/employees");
+const {
+  all,
+  add,
+  remove,
+  employee,
+  edit,
+} = require("../controllers/employees");
 
 // /api/employees
 router.get("/", auth, all);
 // /api/employees/:id
-router.get("/:id", auth, () => console.log("get single employees"));
+router.get("/:id", auth, employee);
 // /api/employees/add
 router.post("/add", auth, add);
 // /api/employees/remove/:id
-router.post("/remove/:id", auth, () => console.log("remove employees"));
+router.post("/remove/:id", auth, remove);
 // /api/employees/edit/:id
-router.put("/edit/:id", auth, () => console.log("edit employees"));
+router.put("/edit/:id", auth, edit);
 
 module.exports = router;
