6c60cbe05da63a886b46aabd4e7e44f7da8170ff
michw85 <mvorontsov713@gmail.com>
2024-12-29 18:49:44 +0100

    react-dependencies
    install libraries and dependencies

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
-import { store } from "./app/store";
-import App from "./App";
+import { store } from "./app/store.ts";
+// import App from "./App.tsx";
 // import reportWebVitals from './reportWebVitals';
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
@@ -11,14 +11,11 @@ const container = document.getElementById("root")!;
 const root = createRoot(container);
 
 root.render(
-  // <React.StrictMode>
-  // <Provider store={store}>
-  // <BrowserRouter>
-  //   <App />
-  // </BrowserRouter>
-  // </Provider>
-  // </React.StrictMode>
-  <div>Hallo Welt</div>
+  <React.StrictMode>
+    <Provider store={store}>
+      <h1>Hello world</h1>
+    </Provider>
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
