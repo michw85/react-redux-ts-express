ba3eb59cf8f66679ea369bfd26b4830df856c327
michw85 <mvorontsov713@gmail.com>
2025-01-12 16:06:55 +0100

    styles
    module.css

--- client/src/components/layout/index.tsx
+++ client/src/components/layout/index.tsx
@@ -1,4 +1,7 @@
 import React from "react";
+import { Layout as AntLayout } from "antd/dist/antd";
+import { Button, DatePicker } from "antd/dist/antd";
+import styles from "./index.module.css";
 
 type Props = {
   children: React.ReactNode;
@@ -6,7 +9,16 @@ type Props = {
 
 // const Layout: React.FC<Props> = ({ children }) => {
 const Layout = ({ children }: Props) => {
-  return <div className={}>{children}</div>;
+  return (
+    <>
+      <div className={styles.main}>
+        <AntLayout.Content style={{ height: "100%" }}>
+          {children}
+        </AntLayout.Content>
+        {/* <Button type="primary">PRESS ME</Button> */}
+      </div>
+    </>
+  );
 };
 
 export default Layout;
